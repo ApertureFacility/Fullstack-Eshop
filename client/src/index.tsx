f6c816570a831fb06207b598a8b97848d3fcae68
Sergey FrontendDeveloper <sergeyt.devwizard@gmail.com>
2025-06-23 18:24:42 +0300

    refactor create layout and change routes with him and without

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
-import App from "./App";
+import ItemsList from "./components/ItemsComonent/ItemsBlock";
+import ItemDetailPage from "./components/ItemsDetailPage/ItemDetailPage";
+import Layout from "./components/Layout/Layout";
+import Auth from "./pages/Auth/Auth";
+import { LOGIN_ROUTE, REGISTRATION_ROUTE } from "./utils/constants";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -12,9 +16,19 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+    <BrowserRouter>
+  <Routes>
+    {/* без Header */}
+    <Route path={LOGIN_ROUTE} element={<Auth />} />
+    <Route path={REGISTRATION_ROUTE} element={<Auth />} />
+
+    {/* все остальные с Layout и Header */}
+    <Route path="/" element={<Layout />}>
+      <Route index element={<ItemsList />} />
+      <Route path="item/:id" element={<ItemDetailPage />} />
+    </Route>
+  </Routes>
+</BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
